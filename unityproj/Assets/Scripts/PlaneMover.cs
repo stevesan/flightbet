7abc634471ef5c279dae7ef4e8a1a24720a65548
Brian Cronin <programmingisgood@gmail.com>
2013-09-28 10:11:08 -0700

    Tweaked controls

--- unityproj/Assets/Scripts/PlaneMover.cs
+++ unityproj/Assets/Scripts/PlaneMover.cs
@@ -5,7 +5,7 @@ public class PlaneMover : MonoBehaviour
 {
     public float throttle = 15f;
     public float maxSpeed = 40f;
-    public float angularSpeed = 2f;
+    public float angularSpeed = 4f;
     public float liftSpeed = 14f;
     public AudioSource thrustAudio;
     public ParticleSystem thrustFx;
@@ -15,19 +15,13 @@ public class PlaneMover : MonoBehaviour
     {
 	}
 
-    private void CheckOnGround()
-    {
-        //var fwd = transform.TransformDirection (Vector3.up);
-        //if (Physics.Raycast (transform.position, fwd, 10)) {
-    }
-
     void FixedUpdate()
     {
-        float angleChange = Input.GetAxisRaw("Vertical");
-        //float stallFactor = Mathf.Clamp(Vector3.Dot(rigidbody.velocity, transform.right), 0, 1);
-        rigidbody.AddTorque(new Vector3(0f, 0f, angleChange * angularSpeed));
+        float angleChange = -Input.GetAxisRaw("Vertical");
+        float stallFactor = Mathf.Clamp(Vector3.Dot(rigidbody.velocity, transform.right), 0, 1);
+        rigidbody.AddTorque(new Vector3(0f, 0f, angleChange * angularSpeed * stallFactor));
 
-        Vector3 move = transform.right * Mathf.Max(0, Input.GetAxisRaw("Horizontal") * throttle);
+        Vector3 move = transform.right * Mathf.Max(0, Input.GetAxisRaw("Fire1") * throttle);
         rigidbody.AddForce(move, ForceMode.Force);
 
         Vector3 liftForce = new Vector3(0, Mathf.Clamp(Vector3.Dot(rigidbody.velocity, transform.right), 0, 1), 0);
@@ -37,8 +31,6 @@ public class PlaneMover : MonoBehaviour
         {
             rigidbody.velocity = rigidbody.velocity.normalized * maxSpeed;
         }
-
-        CheckOnGround();
 
         if( move.magnitude > 1e-2 && !thrustAudio.isPlaying )
         {
